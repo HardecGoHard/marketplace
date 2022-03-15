e47ed8d7e15f704be6db62c8e1972d29a8ae6c0f
Alexey Menshov <aleks7777k2@gmail.com>
2022-03-16 02:37:45 +0300

    fixes

--- src/main/java/com/marketplace/marketplace/repository/UserRepository.java
+++ src/main/java/com/marketplace/marketplace/repository/UserRepository.java
@@ -2,12 +2,13 @@ package com.marketplace.marketplace.repository;
 
 import com.marketplace.marketplace.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUuid(String uuid);
 
-    Optional<User> findByUsername(String uuid);
+    Optional<User> findByUsername(String username);
 
 }
